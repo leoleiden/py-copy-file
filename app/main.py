289d0292d55ce4cf2384568a92248526d8128320
Leonid Lachmann <leocyanid@gmail.com>
2025-06-16 16:58:17 +0300

    replaced the quotation marks

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ def copy_file(command: str) -> None:
     tokens = command.split()
 
     # Перевірка коректності формату команди
-    if len(tokens) != 3 or tokens[0] != 'cp':
+    if len(tokens) != 3 or tokens[0] != "cp":
         return
 
     # Вилучити імена файлів
@@ -16,12 +16,12 @@ def copy_file(command: str) -> None:
 
     try:
         # Відкрити вихідний файл для читання
-        with open(source, 'rb') as file_in:
+        with open(source, "rb") as file_in:
             content = file_in.read()
     except FileNotFoundError:
         # Якщо файл не знайдено - просто вийти без дій
         return
 
     # Записати зчитаний вміст у новий файл
-    with open(dest, 'wb') as file_out:
+    with open(dest, "wb") as file_out:
         file_out.write(content)
